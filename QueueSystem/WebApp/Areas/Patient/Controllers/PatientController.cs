16b184ad81489ee7c30dbee6c91dc8f48ef6f3bf
Piotr Bakun <piotr.bakun@gmail.com>
2019-08-29 15:58:12 +0200

    SignalR work ongoing, adding patient view

---
+++ QueueSystem/WebApp/Areas/Patient/Controllers/PatientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Areas.Patient.Controllers
+{
+    [Area("Patient")]
+    public class PatientController : Controller
+    {
+        [Route("patient/{roomNo}")]
+        public IActionResult Index(string roomNo)
+        {
+            return View();
+        }
+    }
+}
---
+++ QueueSystem/WebApp/wwwroot/js/queueService.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/queueHub").build();
+
+connection.on("ReceiveQueueNo", function (user, message) {
+    console.log(message);
+
+});
+
+connection.start().then().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("PrevNo").addEventListener("click", function (event) {
+    connection.invoke("NewQueueNo", 1, 12).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
