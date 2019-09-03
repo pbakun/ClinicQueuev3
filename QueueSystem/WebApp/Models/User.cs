ba3b960c56a230e001208bb89e33178cba1aaf0a
Piotr Bakun <piotr.bakun@gmail.com>
2019-09-03 15:22:05 +0200

    User class created

---
+++ QueueSystem/WebApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class User
+    {
+        [PrimaryKey, Required, AutoIncrement]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RoomNo { get; set; }
+    }
+}
