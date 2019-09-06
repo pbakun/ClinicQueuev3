746f06d2b5dc1c087c41f5057d71b86cc210619b
Piotr Bakun <piotr.bakun@hotmail.com>
2019-09-06 07:34:55 +0200

    Some HTML changes

--- QueueSystem/WebApp/BackgroundServices/Tasks/ResetQueue.cs
+++ QueueSystem/WebApp/BackgroundServices/Tasks/ResetQueue.cs
@@ -11,7 +11,8 @@ namespace WebApp.BackgroundServices.Tasks
 {
     public class ResetQueue : ScheduledProcessor
     {
-        protected override string Schedule => "*/1 * * * *";
+        //Reset queue every day at 22:00
+        protected override string Schedule => "0 22 * * *";
 
         private IHubContext<QueueHub> _hubContext;
         private readonly IServiceScopeFactory _scopeFactory;
