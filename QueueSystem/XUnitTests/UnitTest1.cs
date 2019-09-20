7154662e311b9b9d367e580d7b1b96cf0cd9aa0a
Piotr Bakun <piotr.bakun@gmail.com>
2019-09-20 11:30:48 +0200

    first test working (CheckNewQueueNo)

--- QueueSystem/XUnitTests/UnitTest1.cs
+++ QueueSystem/XUnitTests/UnitTest1.cs
@@ -1,8 +1,17 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Repository.Interfaces;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WebApp;
+using WebApp.Mappings;
+using WebApp.ServiceLogic;
 using Xunit;
 
 namespace XUnitTests
@@ -32,6 +41,72 @@ namespace XUnitTests
             response.EnsureSuccessStatusCode();
             Assert.Equal("text/html; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData(12)]
+        [InlineData(3)]
+        [InlineData(151)]
+        [InlineData(320)]
+        [InlineData(-775)]
+        [InlineData(-3)]
+        public async void CheckNewQueueNo(int queueNo)
+        {
+            //System.Diagnostics.Debugger.Launch();
+
+            //prepare correct data
+            int dataQueueNo = queueNo;
+            if (queueNo < 0)
+            {
+                dataQueueNo = queueNo * -1;
+            }
+            var preparedData = PrepareData(dataQueueNo).FirstOrDefault();
+            
+            var mockRepo = new Mock<IRepositoryWrapper>();
+            mockRepo.Setup(x => x.Queue.FindByCondition(It.IsAny<Expression<Func<Entities.Models.Queue, bool>>>()))
+                .Returns(PrepareData(dataQueueNo));
+
+            mockRepo.Setup(x => x.Queue.Update(It.IsAny<Entities.Models.Queue>()));
+            mockRepo.Setup(x => x.SaveAsync());
+
+            var mockScopeFactory = new Mock<IServiceScopeFactory>();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            var sut = new QueueService(mockRepo.Object, mapper, mockScopeFactory.Object);
+
+            var result = await sut.NewQueueNo("1", queueNo);
+
+            var queueNoString = queueNo.ToString();
+
+
+            var expectedQueueMessage = String.Concat(preparedData.OwnerInitials, preparedData.QueueNo);
+
+            Assert.Equal(expectedQueueMessage, result.QueueNoMessage);
+            Assert.True(!preparedData.IsBreak);
+            Assert.True(!preparedData.IsSpecial);
+
+        }
+
+        private List<Entities.Models.Queue> PrepareData(int queueNo)
+        {
+            List<Entities.Models.Queue> output = new List<Entities.Models.Queue>();
+            var data = new Entities.Models.Queue()
+            {
+                QueueNo = queueNo,
+                IsBreak = false,
+                IsSpecial = false,
+                OwnerInitials = "PB",
+                RoomNo = 12
+            };
+
+            output.Add(data);
+
+            return output;
         }
     }
 }
